cd31efaf6b6bd96d1d455ae4af16deb343e0be3a
LaurenceLin <laurencelin0614@icloud.com>
2025-04-26 18:34:51 +0800

    Initial Commit

---
+++ Kebuke Ordering App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kebuke Ordering App
+//
+//  Created by 林上閔 on 4/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kebuke Ordering AppTests/Kebuke_Ordering_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kebuke_Ordering_AppTests.swift
+//  Kebuke Ordering AppTests
+//
+//  Created by 林上閔 on 4/26/25.
+//
+
+import Testing
+@testable import Kebuke_Ordering_App
+
+struct Kebuke_Ordering_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
